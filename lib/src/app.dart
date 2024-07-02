7c0edcbb7493c212a4512cfc0d424141e1f2e8ad
MiguelcMartín <miguelmartin97@hotmail.com>
2024-07-02 21:24:52 +0700

    created currency picker screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,4 @@
-import 'package:currency_converter/src/views/home.dart';
+import 'package:currency_converter/src/screens/home.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
